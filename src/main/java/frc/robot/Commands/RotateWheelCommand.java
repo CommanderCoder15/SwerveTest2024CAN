e5a54474ba9f297b66257d96eec76cd9c652b387
CommanderCoder15 <superzoom15.5@gmail.com>
2024-08-19 21:09:43 -0400

    comments (but still wip)

--- src/main/java/frc/robot/Commands/RotateWheelCommand.java
+++ src/main/java/frc/robot/Commands/RotateWheelCommand.java
@@ -15,28 +15,40 @@ public class RotateWheelCommand extends Command {
   XboxController control;
   MathR math;
   
-  final double FLmin = 0.2039;
+  final double FLmin = 0.2039; // motor minimum constants
   final double FRmin = 0.0150;
   final double BRmin = 0.0864;
   final double BLmin = 0.0267;
 
-  final double FLmax = 3.3267;
+  final double FLmax = 3.3267; // motor maximum constants
   final double FRmax = 3.247;
   final double BRmax = 2.96;
   final double BLmax = 3.229;
 
-  final double FLcntr = 1.6;
+  final double FLcntr = 1.6; // motor center constants (points toward center of the robot)
   final double FRcntr = 1.7;
   final double BRcntr = 0.091;
   final double BLcntr = 3.01;
 
-  double FLaimDeg;
+  double FLaimDeg; // degree measure to aim at
+  double FRaimDeg;
+  double BRaimDeg;
+  double BLaimDeg;
 
-  double FLcurrentDeg;
+  double FLcurrentDeg; // Current degrees
+  double FRcurrentDeg;
+  double BRcurrentDeg;
+  double BLcurrentDeg;
 
-  double FLDegDiff;
+  double FLDegDiff; // aim - current
+  double FRDegDiff;
+  double BRDegDiff;
+  double BLDegDiff;
 
-  boolean FLFlip;
+  boolean FLDegFlip; // flips format from 0 to 360 to -180 to 180
+  boolean FRDegFlip;
+  boolean BRDegFlip;
+  boolean BLDegFlip;
 
   public RotateWheelCommand(RotateWheelSubsystem rotateWheel, XboxController control) {
     this.rotateWheel = rotateWheel;
@@ -65,25 +77,25 @@ public class RotateWheelCommand extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    FLcurrentDeg = MathR.lerp(0, 360, FLmin, FLmax, rotateWheel.getFLvalue()) * -1 + 360;
+    // Turn radians into degrees
+    FLcurrentDeg = MathR.lerp(0, 360, FLmin, FLmax, rotateWheel.getFLvalue());
+    FLcurrentDeg = FLcurrentDeg * -1 - 90
+
     FLDegDiff = FLaimDeg - FLcurrentDeg;
-    if(FLDegDiff >= 180){ // detects whether the aiming passes through 
-      FLaimDeg -= 180;
-      FLcurrentDeg -= 180;
-      FLFlip = true;
+    if(FLDegDiff >= 180){ // detects whether optimal aiming passes through 0/360. If so, change from degrees from 0 to 360 to degrees from -180 to 180
+      FLaimDeg -= 180; FLcurrentDeg -= 180;
+      FLDegFlip = true;
       FLDegDiff = FLaimDeg - FLcurrentDeg;
     }
 
-
-
-    if(FLFlip){
+    if(FLDegFlip){
       FLaimDeg += 180;
       FLcurrentDeg += 180;
-      FLFlip = false;
+      FLDegFlip = false;
     }
 
     /* Code to find encoder values (execution)
-    if(control.getRightBumper()) {
+    if(control.getRightBumper() - control.getLeftBumper()) { // turn wheels
       rotateWheel.rotateFLWheel(0.05);
       rotateWheel.rotateFRWheel(0.05);
       rotateWheel.rotateBLWheel(0.05);
@@ -94,7 +106,7 @@ public class RotateWheelCommand extends Command {
       rotateWheel.rotateBLWheel(0);
       rotateWheel.rotateBRWheel(0);
     }
-    if (rotateWheel.getFLvalue() < FLmin) {
+    if (rotateWheel.getFLvalue() < FLmin) { //reports encoder values as they change
       FLmin = rotateWheel.getFLvalue();
       System.out.print("FL min: "); System.out.println(FLmin);
     }
@@ -128,7 +140,7 @@ public class RotateWheelCommand extends Command {
       System.out.print("BR max: "); System.out.println(BRmax);
     }
     
-    if (control.getAButton()) {
+    if (control.getAButton()) { // press A button to output all encoder values
       System.out.print("FL min: "); System.out.println(FLmin);
       System.out.print("FR min: "); System.out.println(FRmin);
       System.out.print("BL min: "); System.out.println(BLmin);
